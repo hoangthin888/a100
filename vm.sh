28240b0955b7e6195c33304948a398488f1f1320
hoangthin888 <102247382+hoangthin888@users.noreply.github.com>
2022-07-27 09:32:09 +0200

    Update vm.sh

--- vm.sh
+++ vm.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-worker="worker_"
-wallet="wallet_"
+loginString=(loginString_)
+group="group_"
 
-cd /
-cd /usr/local/bin
-sudo wget https://github.com/trexminer/T-Rex/releases/download/0.25.9/t-rex-0.25.9-linux.tar.gz
-sudo tar -xf t-rex-0.25.9-linux.tar.gz
-sudo bash -c "echo -e \"[Unit]\\nAfter=network.target\n[Service]\nType=simple\nExecStart=/usr/local/bin/t-rex -a ethash -o stratum+tcp://eth.2miners.com:2020 -u $wallet -p x -w $worker\n[Install]\nWantedBy=multi-user.target\" > /etc/systemd/system/mrun.service"
-sudo systemctl daemon-reload
-sudo systemctl enable mrun.service
-sudo systemctl start mrun.service
+curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+az login --service-principal --username ${loginString[0]} --password ${loginString[1]} --tenant ${loginString[2]};
+while [ 1 ]
+do
+    az vm start --ids $(az vm list -g $group --query "[].id" -o tsv) --no-wait
+    echo "start...."
+    sleep 10
+done
